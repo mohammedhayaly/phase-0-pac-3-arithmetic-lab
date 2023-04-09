25fa78499c910121854c91b37f3178ab6ec31506
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-04-09 17:49:29 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,95 @@
+
+
+
+
+
+
+
+
+function add(a, b) {
+  return (a + b);
+}
+
+add(1, 2); 
+
+
+
+function subtract(a, b) {
+  return (a - b);
+}
+
+subtract(1, 2); 
+
+
+
+function multiply(a, b) {
+  return (a * b);
+}
+
+multiply(1, 2); 
+
+
+
+
+function divide(a, b) {
+  return (a / b);
+}
+
+divide(4,2); 
+
+
+
+
+
+
+let n = 3;
+
+function increment(n){
+
+  n++;
+  
+  return n;
+  
+}
+
+increment();
+
+
+
+
+
+let d = 3;
+
+function decrement(d){
+
+  d--;
+  
+  return d;
+  
+}
+
+increment();
+
+
+
+
+
+function makeInt() {
+  var f = "100";
+  var g = parseInt(f);
+  console.log("Integer value is" + g);
+  var j = parseInt("3 11 43");
+
+  console.log('Integer value is ' + j);
+
+}
+convertStoI();
+
+
+
+
+
+
+
+
+
